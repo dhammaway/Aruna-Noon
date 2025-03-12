00a963726191e798806aef4208ae5b84df2175a8
bNagasena <s3nsei83misc@gmail.com>
2025-03-12 20:23:59 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Aruna-Noon/sw.js', { scope: '/Aruna-Noon/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
